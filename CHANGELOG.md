741a078954452f481b96906d109cb875de9b1d85
Taiki Endo <te316e89@gmail.com>
2019-01-06 20:53:16 +0900

    Release v0.3.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Unreleased
 
+# 0.3.4 - 2019-01-06
+
+* Support futures::AsyncRead and futures::AsyncWrite
+
 # 0.3.3 - 2019-01-04
 
 * Update minimum derive_utils version to 0.5.4
--- src/lib.rs
+++ src/lib.rs
@@ -744,7 +744,7 @@
 //!
 
 #![recursion_limit = "256"]
-#![doc(html_root_url = "https://docs.rs/auto_enums/0.3.3")]
+#![doc(html_root_url = "https://docs.rs/auto_enums/0.3.4")]
 #![no_std]
 
 extern crate auto_enums_core;
