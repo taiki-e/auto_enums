58937835b3cf2101812f2a544387f111f7765102
Taiki Endo <te316e89@gmail.com>
2019-03-13 18:24:24 +0900

    Avoid single_use_lifetimes warning on Rust 1.31

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,7 +4,7 @@
 #![deny(unsafe_code)]
 #![deny(rust_2018_idioms, unreachable_pub)]
 #![deny(clippy::all, clippy::pedantic)]
-#![warn(single_use_lifetimes)]
+// #![warn(single_use_lifetimes)]
 #![warn(clippy::nursery)]
 
 extern crate proc_macro;
