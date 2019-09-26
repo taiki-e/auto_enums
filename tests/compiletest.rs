f29daa6dcaf4f664173a010eb0bf7b39b65f034b
Taiki Endo <te316e89@gmail.com>
2019-09-26 21:30:32 +0900

    Use trybuild instead of compiletest

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -6,30 +6,9 @@
     feature = "try_trait",
 ))]
 
-use std::{env, path::PathBuf};
-
-fn run_mode(mode: &'static str) {
-    let mut config = compiletest::Config::default();
-    config.mode = mode.parse().expect("invalid mode");
-    let mut me = env::current_exe().unwrap();
-    me.pop();
-    config.target_rustcflags = Some(format!(
-        "--edition=2018 \
-         -Z unstable-options \
-         --extern auto_enums \
-         -L {}",
-        me.display()
-    ));
-    let src = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    config.src_base = src.join("tests").join(mode);
-
-    me.pop();
-    me.pop();
-    config.build_base = me.join("tests").join(mode);
-    compiletest::run_tests(&config);
-}
-
 #[test]
-fn compiletest() {
-    run_mode("ui");
+fn ui() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/ui/auto_enum/*.rs");
+    t.compile_fail("tests/ui/enum_derive/*.rs");
 }
