15191f5f30ad949db9614476f13077f136f37f05
Taiki Endo <te316e89@gmail.com>
2020-12-07 17:11:22 +0900

    Add clippy.toml
    Clippy now has an option that specifies MSRV.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -9,15 +9,8 @@
 // It cannot be included in the published code because these lints have false positives in the minimum required version.
 #![cfg_attr(test, warn(single_use_lifetimes))]
 #![warn(clippy::all, clippy::default_trait_access)]
-// mem::take, #[non_exhaustive], and Option::{as_deref, as_deref_mut} require Rust 1.40,
-// matches! requires Rust 1.42, str::{strip_prefix, strip_suffix} requires Rust 1.45
-#![allow(
-    clippy::mem_replace_with_default,
-    clippy::manual_non_exhaustive,
-    clippy::option_as_ref_deref,
-    clippy::match_like_matches_macro,
-    clippy::manual_strip
-)]
+// mem::take requires Rust 1.40
+#![allow(clippy::mem_replace_with_default)]
 
 // older compilers require explicit `extern crate`.
 #[allow(unused_extern_crates)]
