1eb9355082aeb034cb2779b73347da8c3b855be8
Taiki Endo <te316e89@gmail.com>
2021-05-28 22:14:14 +0900

    Apply clippy::wildcard_imports lint

--- src/lib.rs
+++ src/lib.rs
@@ -861,7 +861,7 @@
 #![warn(future_incompatible, rust_2018_idioms, unreachable_pub)]
 // It cannot be included in the published code because these lints have false positives in the minimum required version.
 #![cfg_attr(test, warn(single_use_lifetimes))]
-#![warn(clippy::default_trait_access)]
+#![warn(clippy::default_trait_access, clippy::wildcard_imports)]
 
 #[cfg(all(feature = "generator_trait", not(feature = "unstable")))]
 compile_error!(
