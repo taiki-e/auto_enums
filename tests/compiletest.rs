70694cb2818b901b313b46241a8f45578dbe5cdc
Taiki Endo <te316e89@gmail.com>
2021-08-24 11:28:26 +0900

    Update ui tests to nightly-2021-08-24

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -15,7 +15,7 @@
 
 use std::env;
 
-#[rustversion::attr(not(nightly), ignore)]
+#[rustversion::attr(before(2021-08-23), ignore)] // Note: This date is commit-date and the day before the toolchain date.
 #[test]
 fn ui() {
     if env::var_os("CI").is_none() {
