3bc2d3abc8ea119ac7d55da5a1f75f83849f3227
Taiki Endo <te316e89@gmail.com>
2022-07-30 13:00:43 +0900

    Fix clippy::assertions_on_result_states warning
    ```
    warning: called `assert!` with `Result::is_ok`
       --> tests/auto_enum.rs:457:5
        |
    457 |     assert!(transpose_ok(None).is_ok());
        |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: replace with: `transpose_ok(None).unwrap()`
        |
        = note: `#[warn(clippy::assertions_on_result_states)]` on by default
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#assertions_on_result_states
    
    warning: called `assert!` with `Result::is_ok`
       --> tests/auto_enum.rs:481:5
        |
    481 |     assert!(transpose_result(None).is_ok());
        |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: replace with: `transpose_result(None).unwrap()`
        |
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#assertions_on_result_states
    ```

--- tests/auto_enum.rs
+++ tests/auto_enum.rs
@@ -454,7 +454,7 @@ fn stable_std() {
     fn transpose_ok(file: Option<&Path>) -> io::Result<impl io::Write> {
         if let Some(file) = file { File::create(file) } else { Ok(io::stdout()) }.transpose_ok()
     }
-    assert!(transpose_ok(None).is_ok());
+    transpose_ok(None).unwrap();
 
     #[auto_enum(Transpose, Write)]
     fn transpose_option(file: Option<&Path>) -> Option<impl io::Write> {
@@ -478,7 +478,7 @@ fn stable_std() {
         }
         .transpose()
     }
-    assert!(transpose_result(None).is_ok());
+    transpose_result(None).unwrap();
 
     #[auto_enum(Transpose, Debug, Display, Error)]
     fn transpose_err(file: Option<&Path>) -> Result<(), impl Error> {
