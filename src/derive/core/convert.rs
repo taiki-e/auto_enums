204f98ec88a637d472f195505ca523444a51bd8e
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-02-14 13:54:07 +0100

    Add support for `Into` derive

--- src/derive/core/convert.rs
+++ src/derive/core/convert.rs
@@ -29,3 +29,42 @@ pub(crate) mod as_mut {
         }))
     }
 }
+
+pub(crate) mod into {
+    use syn::{spanned::Spanned as _, Error, PathArguments};
+
+    use crate::derive::prelude::*;
+
+    pub(crate) const NAME: &[&str] = &["Into"];
+
+    pub(crate) fn derive(cx: &Context, data: &Data) -> Result<TokenStream> {
+        let Some(path) = cx.trait_path() else { unreachable!() };
+        let Some(trait_name) = path.segments.last() else { unreachable!() };
+
+        let PathArguments::AngleBracketed(ref into_type) = trait_name.arguments else {
+            return Err(Error::new(
+                path.span(),
+                "Into trait requires a generic argument, eg: Into<TargetType>.",
+            ));
+        };
+        if into_type.args.len() != 1 {
+            return Err(Error::new(into_type.span(), "Into trait must take one argument."));
+        }
+        let target = into_type.args.first().unwrap().clone();
+        let path = path.clone();
+
+        let mut enum_impl = derive_utils::EnumImpl::new(data);
+        enum_impl.set_trait(path.clone());
+        for v in &data.variants {
+            let variant_name = &v.ident;
+            enum_impl.push_where_predicate(parse_quote! {
+                #variant_name: #path
+            });
+        }
+        enum_impl.push_method(parse_quote! {
+            #[inline]
+            fn into(self) -> #target;
+        });
+        Ok(enum_impl.build())
+    }
+}
